12a40f718c43485d02306d7744e92fd8bfe483fe
Даниил Савко <zzoddiaak@gmail.com>
2025-02-14 17:07:43 +0300

    Create module

---
+++ passenger-service/src/test/java/passenger_service/passenger_service/PassengerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package passenger_service.passenger_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassengerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ rides-service/src/main/java/rides_service/rides_service/RidesServiceApplication.java
@@ -0,0 +1,13 @@
+package rides_service.rides_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RidesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RidesServiceApplication.class, args);
+	}
+
+}
