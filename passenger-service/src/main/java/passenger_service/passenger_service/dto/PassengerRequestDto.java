9906f38b42834cee68c15334b53d4b1c7b7bb302
Даниил Савко <zzoddiaak@gmail.com>
2025-02-17 18:18:27 +0300

    Create DB, dto and entity. Add liquibase

---
+++ passenger-service/src/main/java/passenger_service/passenger_service/dto/PassengerRequestDto.java
@@ -0,0 +1,19 @@
+package passenger_service.passenger_service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PassengerRequestDto {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Integer phoneNumber;
+
+}
---
+++ passenger-service/src/main/java/passenger_service/passenger_service/entity/Passenger.java
@@ -0,0 +1,32 @@
+package passenger_service.passenger_service.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Setter
+@Getter
+@Table(name = "passengers")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Passenger {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "phone_number", nullable = false)
+    private Integer phoneNumber;
+
+
+}
